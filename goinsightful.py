579d4a8184e7ba5a57b9ed93db27121200c0014d
ronakagarwal3434 <60756098+ronakagarwal3434@users.noreply.github.com>
2021-02-27 19:50:32 +0530

    make it better

---
+++ goinsightful.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/contactus")
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
