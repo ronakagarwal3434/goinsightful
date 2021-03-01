06611d005a0f913e123366020dbf20313dfb47d1
ronakagarwal3434 <60756098+ronakagarwal3434@users.noreply.github.com>
2021-03-01 23:18:20 +0530

    final commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm, LoginForm
 import gunicorn
 
@@ -9,12 +9,16 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
-@app.route("/contactus")
+@app.route("/contactus", methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+	if request.method == 'POST':
+		flash('Your details have been successfully updated!', 'success')
+		return render_template('contact.html')
+	else:
+		return render_template('contact.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
